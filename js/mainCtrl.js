04cf8b964109e47594c9efc300c0580ddcd93d2b
Joseph Pimentel <I59307@xactware.com>
2017-02-07 19:30:12 -0700

    mini project mostly complete except for the ambitious part.

---
+++ js/mainCtrl.js
@@ -0,0 +1,10 @@
+angular.module('friendsList').controller('mainCtrl', function ($scope) {
+  $scope.friends = [
+    'Ian',
+    'Josh',
+    'Tanner',
+    'Jeremiah',
+    'Zach',
+    'Dan'
+  ];
+});
